6f9747a2b155acc66e2b13ed9ec030fb0c5b41f8
junkey <thong01687300206@gmail.com>
2020-06-28 12:01:36 +0700

    + Edit scipt + Add exclude file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rsyncBackupSystem
 This script will backup whole OS (everething) to another backup disk
+list exlcude file from exclude.txt
+Todo:
++ Already verify on VM
++ Add to cron jobs
--- backup.sh
+++ backup.sh
@@ -4,22 +4,19 @@
 
 ############ Some global setting ############
 # Source directory we wish to backup
-source_dir='src_test'
+source_dir='/'
 
 # Destination directory we wish to backup to (DO NOT end with a forward-slash).
 dest_dir='/HDD500'
 
-# We will use date as name of backup folder name
-backup_name=$(date +"%d-%m-%Y")
-
 # Log file for this script
 logfile='/var/log/rsync_backup'
 
-# Exclude folder and file
-exclude_folder_and_file=${dest_dir,'/.gvfs/','/Examples/','/.local/share/Trash/','/.thumbnails/', \
-               '/transient-items/','/dev/*','/proc/*','/sys/*','/tmp/*','/mnt/*','/media/*', \
-               '/run/*','/lost+found','/home/*/.cache/mozilla/*','/home/*/.cache/chromium/*', \
-               '/home/*/.local/share/Trash/*','swapfile','.cache'}
+# Disk UUID
+diskUUID=500cfbf9-41f3-4b3e-98fd-53a9ed287a69
+
+# Exclude folder and file. Should be list on 1 line, otherwise parsing will be wrong
+exclude_folder_and_file={$dest_dir,'dev/*','lost+found','mnt/*','/proc/*','run/*','sys/*','tmp/*','home/*/.gvfs/','.thumbnails/','media','home/*/.cache/mozilla/','home/*/.cache/chromium/','home/*/.local/share/Trash/','swapfile','.cache'}
 
 #############################################
 
@@ -27,63 +24,46 @@ writeToLog() {
 	echo -e "$(date --rfc-3339=seconds) ${1}" | tee -a "${logfile}"
 }
 
+# Renew log file
+echo '' > $logfile
 writeToLog "########## FULL SYSTEM BACKUP ##########"
 
 # Check if backup directory is exist
 if [ ! -d $dest_dir ]; then
 	writeToLog "$dest_dir does NOT exist, creating ... "
-	sudo mkdir -p $dest_dir
+	mkdir -p $dest_dir
 fi 
 
 # Check whether target volume is mounted, and mount it if not
 if ! mountpoint -q $dest_dir/; then
         writeToLog "Mounting the external drive to: $dest_dir ..."
-        if ! sudo mount $dest_dir && ! mountpoint -q $dest_dir/; then
+        if ! mount -U $diskUUID $dest_dir && ! mountpoint -q $dest_dir/; then
                 writeToLog "FAILED to mount! Exiting ..."
                 exit 1
         fi
 fi
 
-# TODO:
-# How to rotate log file, or copy, or push to message
-# how to find disk to mount if label is change ?
-# ask for root password ?
-# delete folder when reach threshol
-
-if [ ! -d $dest_dir/$backup_name ]; then
-	writeToLog "Creating the backup folder: $backup_name"
-	sudo mkdir -p $dest_dir/$backup_name
-fi
-
 # Let's back up
 writeToLog "Starting backup system FROM $source_dir TO $dest_dir ..."
-if sudo rsync --archive --acls --xattrs --partial --progress --verbose --human-readable \
-              --itemize-changes --progress --dry-run --delete --delete-excluded \
-              --exclude=$exclude_folder_and_file $source_dir $dest_dir \
-              --log-file=$logfile
+if rsync --archive --acls --xattrs --partial --human-readable --recursive --hard-links --owner \
+		--delete --delete-excluded --info=progress2 --verbose \
+		--exclude-from=exclude.txt $source_dir $dest_dir \
+		--log-file=$logfile
 then
 	writeToLog "Backup completed successfully"
 else
 	writeToLog "Backup FAILED !!!"
 fi
 
-# We only keep 5 latest backup
-number_backup=$(ls -l $dest_dir | grep "^d" | wc -l)
-if [ $number_backup -gt 5 ]; then
-	delete_dirs=$(sudo find ${dest_dir} - type d -printf '%T+ %p\n' | sort  | tail -n + 6 | awk '{print $NF}')
-	writeToLog "Reached threshold! Deleting: $delete_dirs"
-	sudo find $dest_dir - type d -printf '%T+ %p\n' | sort  | tail -n + 6 | awk '{print $NF}' | xargs rm -rf
-fi
-
 writeToLog "Un-mount the mountpoint ..."
-if ! sudo umount $dest_dir; then
+if ! umount $dest_dir; then
         writeToLog "FAILED to umount $dest_dir!"
 else
 	# Delete destination directory
 	writeToLog "Remove the directory: $dest_dir after ummount the disk ..."
 	if [ -d $dest_dir ]; then
-		sudo rm -rf $dest_dir
+		rm -rf $dest_dir
 	fi
 fi
-writeToLog "backup DONE!"
+writeToLog "Backup DONE!"
 exit 0
